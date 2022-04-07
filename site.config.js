28ef7d57affcded2be27be78305b53f843e610d0
Goya <74860630+script-money@users.noreply.github.com>
2022-04-07 20:00:22 +0800

    🔧 modify config

--- site.config.js
+++ site.config.js
@@ -1,23 +1,23 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f4e9b8114a4d4da3944097534bf345ff',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'CryptoChasers',
+  domain: 'cryptochasers.co',
+  author: 'script money',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'A DAO helping new Web3 projects and users grow',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'cryptochasersco',
+  github: 'crypto-chasers',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
